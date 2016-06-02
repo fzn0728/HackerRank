dcd8abea1985fdeb6c2f0b5f7ccbfecf2aca3130
Chandler <thames.ft@gmail.com>
2016-06-01 23:50:46 -0700

    Create Find the Second_Largest_Number.py

---
+++ Find the Second_Largest_Number.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+i = int(input())
+lis = list(map(int,raw_input().strip().split()))[:i] # It is so powerful to use map(func,seq) and strip(), split() to fetch info from strings
+z = max(lis)
+while max(lis) == z:
+    lis.remove(max(lis))
+
+print max(lis)
