fe63fb09a0cb4906e761ee683ca892ebd14d6de6
Chandler <thames.ft@gmail.com>
2016-06-02 00:00:13 -0700

    Add third method

--- Find the Second_Largest_Number.py
+++ Find the Second_Largest_Number.py
@@ -13,3 +13,20 @@ N = int(input())
 num = map(int,raw_input().split())
 print sorted(list(set(num)))[-2]
 '''
+
+
+
+'''
+# The third and most general one
+
+n = int(input())
+numb = input()
+lis = list(map(int, numb.split()))
+big, sbig = -6000, -6000
+for i in lis:
+    if (i > big):
+        big, sbig = i, big
+    elif (i < big and i > sbig):
+        sbig = i
+print (sbig)
+'''
